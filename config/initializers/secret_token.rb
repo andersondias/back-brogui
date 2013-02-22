7a66069b7c1ccb9d092177ec04a9881a19380130
Anderson Dias <andersondaraujo@gmail.com>
2013-02-22 15:10:04 -0300

    initial version

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Brogui::Application.config.secret_token = 'fde669d497b509e3721d59e3f72a2a7909844189c9a5ee0cf6f20ed4913ebd83a54a765372547862e8fc0bdee5ffaf1f82cbf3e61f100a9d601649550eb88e08'
